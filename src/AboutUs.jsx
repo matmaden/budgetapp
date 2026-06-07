eafa96a96572decd77d960f08a42914216f83dca
Johnny_truong <lttruongx4@gmail.com>
2026-06-07 15:51:51 +0700

    Add files via upload

---
+++ src/AboutUs.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div className="about-us">
+      <h1>About Us</h1>
+      <p>
+        Welcome to our company. We are dedicated to providing high-quality
+        products and services that help our customers achieve their goals.
+      </p>
+      <p>
+        Our mission is to deliver innovative solutions, exceptional customer
+        service, and lasting value through technology and continuous improvement.
+      </p>
+      <p>
+        Founded by passionate professionals, our team works together to create
+        reliable, user-friendly experiences for customers around the world.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
